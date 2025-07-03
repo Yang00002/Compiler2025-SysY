be107d17496480de5480d113ddf0c12a5eb17a4b
请输入队名 <t202510358205721@eduxiji.net>
2025-07-03 17:54:17 +0800

    修正了参数解析和文件命名。

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -7,6 +7,7 @@
 #include "../include/codegen/ARM_codegen.hpp"
 
 #include <CharStream.h>
+#include <cstdlib>
 #include <tree/ParseTree.h>
 #include <tree/ParseTreeVisitor.h>
 #include <tree/ParseTreeWalker.h>
@@ -18,34 +19,29 @@
 
 std::tuple<std::string, std::string, bool> parseArgs(int argc, char** argv)
 {
-    // compiler <testcase.sy> -S -o <testcase.s> [-O1]
+    // compiler -S -o <testcase.s> <testcase.sy> [-O1]
      if (argc < 5) {
-        std::cerr << "Usage: " << argv[0] << " <testcase.sy> -S -o <testcase.s> [-O1]\n";
+        std::cerr << "Usage: " << argv[0] << "-S -o <testcase.s> <testcase.sy> [-O1]\n";
         std::exit(EXIT_FAILURE);
     }
     int i = 1;
     bool opt = false;
-    auto input_filename = argv[i++];
-    if (std::string(argv[i++]) != "-S") {
-        std::cerr << "Expected -S after input.\n";
-        std::exit(EXIT_FAILURE);
-    }
-    if (std::string(argv[i++]) != "-o") {
-        std::cerr << "Expected -o for output file.\n";
-        std::exit(EXIT_FAILURE);
-    }
-    if (i >= argc) {
-        std::cerr << "Missing output file.\n";
-        std::exit(EXIT_FAILURE);
-    }
-    auto output_filename = argv[i++];
-    if (i < argc && std::string(argv[i]) == "-O1") {
-        opt = true;
-        ++i;
-    }
-    if (i != argc) {
-        std::cerr << "Too many arguments provided.\n";
-        std::exit(EXIT_FAILURE);
+    std::string input_filename, output_filename;
+    for(int i = 1; i < argc; ++i){
+        std::string arg = argv[i];
+        if(arg=="-S")
+            continue;
+        if(arg=="-o"){
+            if( i + 1 < argc )
+                output_filename = argv[++i];
+            else {
+                std::cerr << "Missing output filename.\n";
+                std::exit(EXIT_FAILURE);
+            }
+        } else if(arg=="-O1")
+            opt = true;
+        else
+            input_filename = arg;
     }
     return std::make_tuple(input_filename,output_filename,opt);
 }
