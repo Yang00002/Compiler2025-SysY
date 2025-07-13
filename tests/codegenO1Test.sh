5604892df59d298fec88224311df7337e7f06d3a
请输入队名 <t202510358205721@eduxiji.net>
2025-07-13 12:18:13 +0800

    加入不同 compiler 测试脚本

---
+++ tests/codegenO1Test.sh
@@ -0,0 +1,7 @@
+source_file="$1"
+target_file="$2"
+
+mid_file_s="${target_file}.s"
+
+./build/compiler -S -o "$mid_file_s" "$source_file" -O1
+aarch64-linux-gnu-gcc "$mid_file_s"  -L build/lib -l:sylib.a -static -g -o "$target_file"
